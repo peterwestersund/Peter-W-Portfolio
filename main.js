2d11d44e62ec6c2fbd55d5ef9daddcd5548d9242
peterwestersund <peterwestersund@hotmail.com>
2022-10-07 12:29:52 +0300

    update

---
+++ main.js
@@ -0,0 +1,27 @@
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
+  const element = document.querySelector('.pie');
+  let observer = new IntersectionObserver(entries => {
+  element.classList.toggle('animate', entries[0].isIntersecting
+   );
+
+  
+  });
+  
+  observer.observe( element );
